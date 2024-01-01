5be4e8df6a09da11bcf331f76dc869f45ff51e56
Ben <ben@alt-design.net>
2024-01-01 20:14:26 +0000

    Add option in

--- src/Tags/Collection.php
+++ src/Tags/Collection.php
@@ -2,6 +2,8 @@
 
 namespace AltDesign\AltPasswordProtect\Tags;
 
+use AltDesign\AltPasswordProtect\Helpers\Data;
+
 class Collection extends \Statamic\Tags\Collection\Collection
 {
     /**
@@ -11,8 +13,12 @@ class Collection extends \Statamic\Tags\Collection\Collection
     {
         $this->params['from'] = $this->method;
 
-        $this->params['protect:not'] = 'alt_password_protect_custom';
-        $this->params['protect:empty'] = true;
+        // Check whether we're removing the protected entries
+        $theData = new Data('settings');
+        if($theData->get('hide_password_protected_entries_from_listings') == true) {
+            $this->params['protect:not'] = 'alt_password_protect_custom';
+            $this->params['protect:empty'] = true;
+        }
 
         return $this->output(
             $this->entries()->get()
